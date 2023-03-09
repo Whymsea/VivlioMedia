8255cdbbab2087251bc1f22a02415bd8547bedfd
Anna <annamathis8585@gmail.com>
2023-03-09 14:52:50 +0100

    Include footer et header + images dans le dossier

---
+++ header.php
@@ -0,0 +1,190 @@
+<!--========== Header ==============-->
+<header id="gen-header" class="gen-header-style-1 gen-has-sticky">
+  <div class="gen-bottom-header">
+     <div class="container">
+        <div class="row">
+           <div class="col-lg-12">
+              <nav class="navbar navbar-expand-lg navbar-light">
+                 <a class="navbar-brand" href="#">
+                    <img class="img-fluid logo" src="images/logo.png" alt="streamlab-image">
+                 </a>
+                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <div id="gen-menu-contain" class="gen-menu-contain">
+                       <ul id="gen-main-menu" class="navbar-nav ml-auto">
+                          <li class="menu-item active">
+                             <a href="#" aria-current="page">Film</a>
+                             <i class="fa fa-chevron-down gen-submenu-icon"></i>
+
+                             <ul class="sub-menu">
+                                <li class="menu-item menu-item-has-children">
+                                   <a href="#">Catégorie</a>
+                                   <i class="fa fa-chevron-down gen-submenu-icon"></i>
+                                   <ul class="sub-menu">
+                                      <li class="menu-item">
+                                         <a href="movies-load-more.html">Action</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-infinite-scroll.html">Aventure</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Suspense</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Thriller</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Horreur</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Comédie</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Fantastique</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Science Fiction</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Documentaire</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Romance</a>
+                                      </li>
+                                   </ul>
+                                </li>
+
+                                
+                                <li class="menu-item menu-item-has-children">
+                                   <a href="#">Ma Liste</a>
+                                   <i class="fa fa-chevron-down gen-submenu-icon"></i>
+                                </li>
+                             </ul>
+
+                          </li>
+
+                          <li class="menu-item">
+                             <a href="tv-shows-home.html">Livre</a>
+                             <i class="fa fa-chevron-down gen-submenu-icon"></i>
+                             
+                             <ul class="sub-menu">
+                                <li class="menu-item menu-item-has-children">
+                                   <a href="#">Catégorie</a>
+                                   <i class="fa fa-chevron-down gen-submenu-icon"></i>
+                                   <ul class="sub-menu">
+                                      <li class="menu-item">
+                                         <a href="movies-load-more.html">Action</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-infinite-scroll.html">Aventure</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Suspense</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Thriller</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Horreur</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Comédie</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Fantastique</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Science Fiction</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Documentaire</a>
+                                      </li>
+                                      <li class="menu-item">
+                                         <a href="movies-pagination.html">Romance</a>
+                                      </li>
+                                   </ul>
+                                </li>
+
+                                
+                                <li class="menu-item menu-item-has-children">
+                                   <a href="#">Ma Liste</a>
+                                   <i class="fa fa-chevron-down gen-submenu-icon"></i>
+                                </li>
+                             </ul>
+                             
+                          </li>
+                      
+                       </ul>
+                    </div>
+                 </div>
+                 <div class="gen-header-info-box">
+                    <div class="gen-menu-search-block">
+                       <a href="javascript:void(0)" id="gen-seacrh-btn"><i class="fa fa-search"></i></a>
+                       <div class="gen-search-form">
+                          <form role="search" method="get" class="search-form" action="#">
+                             <label>
+                                <span class="screen-reader-text"></span>
+                                <input type="search" class="search-field" placeholder="Search …" value="" name="s">
+                             </label>
+                             <button type="submit" class="search-submit"><span
+                                   class="screen-reader-text"></span></button>
+                          </form>
+                       </div>
+                    </div>
+                    <div class="gen-account-holder">
+                       <a href="javascript:void(0)" id="gen-user-btn"><i class="fa fa-user"></i></a>
+                       <div class="gen-account-menu">
+                          <ul class="gen-account-menu">
+                             <!-- Pms Menu -->
+                             <li>
+                                <a href="log-in.html"><i class="fas fa-sign-in-alt"></i>
+                                   login </a>
+                             </li>
+                             <li>
+                                <a href="register.html"><i class="fa fa-user"></i>
+                                   Register </a>
+                             </li>
+                             <!-- Library Menu -->
+                             <li>
+                                <a href="library.html">
+                                   <i class="fa fa-indent"></i>
+                                   Library </a>
+                             </li>
+                             <li>
+                                <a href="library.html"><i class="fa fa-list"></i>
+                                   Movie Playlist </a>
+                             </li>
+                             <li>
+                                <a href="library.html"><i class="fa fa-list"></i>
+                                   Tv Show Playlist </a>
+                             </li>
+                             <li>
+                                <a href="library.html"><i class="fa fa-list"></i>
+                                   Video Playlist </a>
+                             </li>
+                             <li>
+                                <a href="upload-video.html"> <i class="fa fa-upload"></i>
+                                   Upload Video </a>
+                             </li>
+                          </ul>
+                       </div>
+                    </div>
+                    <div class="gen-btn-container">
+                       <a href="register.html" class="gen-button">
+                          <div class="gen-button-block">
+                             <span class="gen-button-line-left"></span>
+                             <span class="gen-button-text">Subscribe</span>
+                          </div>
+                       </a>
+                    </div>
+                 </div>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fas fa-bars"></i>
+                 </button>
+              </nav>
+           </div>
+        </div>
+     </div>
+  </div>
+</header>
